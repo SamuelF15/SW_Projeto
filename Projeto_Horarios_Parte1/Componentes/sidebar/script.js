b91100f295d9fa5c05d3b4fd210cb0027bc12332
Samuel Figueiredo <samuel.lobo2004@gmail.com>
2025-03-10 19:04:27 -0300

    sidebar

---
+++ Projeto_Horarios_Parte1/Componentes/sidebar/script.js
@@ -0,0 +1,11 @@
+function toggleSidebar() {
+    let sidebar = document.getElementById("sidebar");
+    let sidebarWidth = window.innerWidth <= 768 ? "-200px" : "-250px"; // Ajusta dinamicamente
+
+    // Se o menu estiver fechado, abre; se estiver aberto, fecha
+    if (sidebar.style.left === sidebarWidth || sidebar.style.left === "") {
+        sidebar.style.left = "0";
+    } else {
+        sidebar.style.left = sidebarWidth;
+    }
+}
